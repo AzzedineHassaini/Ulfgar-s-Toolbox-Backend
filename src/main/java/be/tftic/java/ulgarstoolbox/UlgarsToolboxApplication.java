27801ca15868b6c1bba1a1f3e119b16f31dbd88c
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-07-22 11:02:23 +0200

    Initial commit

---
+++ src/main/java/be/tftic/java/ulgarstoolbox/UlgarsToolboxApplication.java
@@ -0,0 +1,13 @@
+package be.tftic.java.ulgarstoolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlgarsToolboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UlgarsToolboxApplication.class, args);
+    }
+
+}
