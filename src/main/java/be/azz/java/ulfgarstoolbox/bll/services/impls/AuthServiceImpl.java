1712f692e9b93851f7af708082fbb187b97752e1
AzzedineHassaini <azzedinehassaini@gmail.com>
2024-07-23 16:47:26 +0200

    Unused import

--- src/main/java/be/azz/java/ulfgarstoolbox/bll/services/impls/AuthServiceImpl.java
+++ src/main/java/be/azz/java/ulfgarstoolbox/bll/services/impls/AuthServiceImpl.java
@@ -19,8 +19,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements UserDetailsService, IAuthService {
