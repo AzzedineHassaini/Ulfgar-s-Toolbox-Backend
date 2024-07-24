ab0227ad283f861d2c53dfa9dd922976515136fa
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-07-24 12:30:28 +0200

    Refacto : typo project name

--- src/main/java/be/azz/java/ulfgarstoolbox/UlfgarsToolboxApplication.java
+++ src/main/java/be/azz/java/ulfgarstoolbox/UlfgarsToolboxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UlgarsToolboxApplication {
+public class UlfgarsToolboxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UlgarsToolboxApplication.class, args);
+        SpringApplication.run(UlfgarsToolboxApplication.class, args);
     }
 
 }
